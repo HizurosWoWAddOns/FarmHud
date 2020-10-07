eab9bec0f83a70e18270c1cd35a4453f8c230d19
Hizuro <hizuro@gmx.net>
2020-10-07 04:06:22 +0200

    udpate FarmHud_QuestArrow for shadowlands

--- QuestArrow/FarmHud_QuestArrow.lua
+++ QuestArrow/FarmHud_QuestArrow.lua
@@ -32,7 +32,12 @@ do
 	end
 end
 
-local SetSuperTrackedQuestID_Orig,TrackedQuestID = SetSuperTrackedQuestID
+local GetSuperTrackedQuestID,SetSuperTrackedQuestID_Orig,TrackedQuestID = GetSuperTrackedQuestID,SetSuperTrackedQuestID;
+
+if C_SuperTrack then
+	GetSuperTrackedQuestID = C_SuperTrack.GetSuperTrackedQuestID;
+	SetSuperTrackedQuestID_Orig = C_SuperTrack.SetSuperTrackedQuestID;
+end
 
 local function SetSuperTrackedQuestID_Replacement(questID)
 	questID = tonumber(questID) or 0;
@@ -47,12 +52,20 @@ function FarmHud_ToggleSuperTrackedQuest(token,state)
 	if state==nil then return; end
 	local msg, currentID = false,GetSuperTrackedQuestID();
 	if state and currentID~=0 then
-		SetSuperTrackedQuestID = SetSuperTrackedQuestID_Replacement;
+		if C_SuperTrack then
+			C_SuperTrack.SetSuperTrackedQuestID = SetSuperTrackedQuestID_Replacement;
+		else
+			SetSuperTrackedQuestID = SetSuperTrackedQuestID_Replacement;
+		end
 		TrackedQuestID = GetSuperTrackedQuestID();
 		SetSuperTrackedQuestID_Orig(0);
 		msg = "QuestArrowInfoMsgDisabled";
 	elseif state==false and tonumber(TrackedQuestID) and TrackedQuestID>0 then
-		SetSuperTrackedQuestID = SetSuperTrackedQuestID_Orig;
+		if C_SuperTrack then
+			C_SuperTrack.SetSuperTrackedQuestID = SetSuperTrackedQuestID_Orig;
+		else
+			SetSuperTrackedQuestID = SetSuperTrackedQuestID_Orig;
+		end
 		SetSuperTrackedQuestID_Orig(TrackedQuestID);
 		msg = "QuestArrowInfoMsgRestored";
 	end
