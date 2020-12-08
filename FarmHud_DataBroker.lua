5afae855623c7cdf06c0ca1842d4c53a456f3e13
Hizuro <hizuro@gmx.net>
2020-12-08 06:54:58 +0100

    added option to toggle background with minimap button

--- FarmHud_DataBroker.lua
+++ FarmHud_DataBroker.lua
@@ -12,9 +12,12 @@ function ns.RegisterDataBroker()
 			tt:AddLine(addon);
 			tt:AddLine(("|cffffff00%s|r %s"):format(KEY_BUTTON1,L["DataBrokerToggle"]));
 			tt:AddLine(("|cffffff00%s|r %s"):format(KEY_BUTTON2,L["DataBrokerOptions"]));
+			tt:AddLine(("|cffffff00%s|r %s"):format(SHIFT_KEY.."+"..KEY_BUTTON1,L["DataBrokerToggleBackground"]));
 		end,
 		OnClick = function(_, button)
-			if button=="LeftButton" then
+			if button=="LeftButton" and IsShiftKeyDown() then
+				FarmHud:ToggleBackground();
+			elseif button=="LeftButton" then
 				FarmHud:Toggle();
 			else
 				FarmHud:ToggleOptions();
