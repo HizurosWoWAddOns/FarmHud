1690428f5c0e3fd2de0f5da91d2dc7743fe63181
Hizuro <hizuro@gmx.net>
2022-05-02 19:08:46 +0200

    minor changes in localization

--- Localization.lua
+++ Localization.lua
@@ -1,55 +1,35 @@
 
-local addon, ns = ...
+local L, addon, ns = {}, ...;
 
-local L = {};
 ns.L = setmetatable(L,{__index=function(t,k)
-	local v = tostring(k)
-	rawset(t,k,v)
-	return v
+	local v = tostring(k);
+	rawset(t,k,v);
+	return v;
 end});
 
 -- Do you want to help localize this addon?
--- https://www.curseforge.com/wow/addons/farmhud/localization
+-- https://www.curseforge.com/wow/addons/@cf-project-name@/localization
 
 --@localization(locale="enUS", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-
 if LOCALE_deDE then
 --@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_esES then
+elseif LOCALE_esES then
 --@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_esMX then
+elseif LOCALE_esMX then
 --@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_frFR then
+elseif LOCALE_frFR then
 --@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_itIT then
+elseif LOCALE_itIT then
 --@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_koKR then
+elseif LOCALE_koKR then
 --@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_ptBR or LOCALE_ptPT then
+elseif LOCALE_ptBR or LOCALE_ptPT then
 --@localization(locale="ptBR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_ruRU then
+elseif LOCALE_ruRU then
 --@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_zhCN then
+elseif LOCALE_zhCN then
 --@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-end
-
-if LOCALE_zhTW then
+elseif LOCALE_zhTW then
 --@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 end
 
